8742b3aa8977edbc3f657e0d190124da9b299463
ThePaulCraft <61984114+ThePaulCraft@users.noreply.github.com>
2021-12-23 15:00:15 +0100

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,19 @@
 '''Achtung! Funktioniert nicht!'''
+def to_ascii(char):
+    return chr(int(char, 2))
+
+# öffnet die datei mit dem cyber märchen
+with open('1.txt') as file:
+    text = file.read()
+
+words = text.split(' ')
+
+cyber_words = [w for w in words if 'Cyber' in w]
+
+binary = ''.join(['1' if '-' in c else '0' for c in cyber_words])
+
+binary = ''.join([c+' ' if i % 7 == 0 and i >= 7 else c for i, c in enumerate(binary)])
+
+binary_chars = binary.split(' ')
+password = ''.join([to_ascii(bc) for bc in binary_chars])
+print(f'the password: {password}')
